dacd6e0fd9950790c999d123eaa3589c5d6ed277
Fernando <fernando93d@gmail.com>
2020-09-19 15:01:47 -0500

    Revert "Scaffold users"
    This reverts commit cf51d4d60b3368fd522b29fcb744c8974e9aae44.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-    enum role: [:admin, :student, :teacher]
+  enum role: [:admin, :student, :teacher]
 
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
-    
-    has_many :courses
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  has_many :courses
 end
