cf51d4d60b3368fd522b29fcb744c8974e9aae44
Fernando <fernando93d@gmail.com>
2020-09-19 14:52:13 -0500

    Scaffold users

--- db/migrate/20200918165407_devise_create_users.rb
+++ db/migrate/20200918165407_devise_create_users.rb
@@ -15,6 +15,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Rememberable
       t.datetime :remember_created_at
       t.integer :role, default: 0, null: false
+
+      # t.string :matricula, default: "", null: true
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
