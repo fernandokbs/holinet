f72c3f49e41c0724535ef25fd9ed5b737c6f6132
Fernando <fernando93d@gmail.com>
2020-09-20 15:43:28 -0500

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,5 @@
 module ApplicationHelper
     def my_home
-        if current_user.admin?
-            admin_home_index_path
-        elsif current_user.student?
-            student_home_index_path
-        else
-            teacher_home_index_path
-        end
+        
     end
 end
