db17212007ca11b317ed3a287200e992dd32c652
Fernando <fernando93d@gmail.com>
2020-09-20 15:33:25 -0500

    asdf

--- db/migrate/20200920154110_create_students.rb
+++ db/migrate/20200920154110_create_students.rb
@@ -2,9 +2,7 @@ class CreateStudents < ActiveRecord::Migration[6.0]
   def change
     create_table :students do |t|
       t.string :name
-      t.string :email
       t.string :matricula
-      t.string :password
       t.integer :role, null: false, default: 1
 
       t.timestamps
--- db/migrate/20200920164310_create_teachers.rb
+++ db/migrate/20200920164310_create_teachers.rb
@@ -3,7 +3,6 @@ class CreateTeachers < ActiveRecord::Migration[6.0]
     create_table :teachers do |t|
       t.string :name
       t.string :matricula
-      t.string :mail
       t.integer :role, null: false, default: 2
 
       t.timestamps
