17ab3ebe0c4f6bc11867f139a6fa06059fd9372f
Fernando <fernando93d@gmail.com>
2020-09-20 16:13:27 -0500

    styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
-    def my_home
-        
+    def custom_signed_in?
+        user = if current_user then current_user else  nil end 
+        user = if current_student then current_student else  nil  end
+        user = if current_teacher then current_teacher else  nil  end
+        user
     end
 end
