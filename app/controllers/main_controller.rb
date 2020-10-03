99b4167c8c9452bb7d1bff63b8beb761c52178ce
Fernando <fernando93d@gmail.com>
2020-10-03 17:28:37 -0500

    small changes

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,24 @@
 class MainController < ApplicationController
+    include ApplicationHelper
+
+    before_action :require_login, only: [:main]
+
     def main
+
     end
 
     def home
+    end
 
+    private
+
+    def auth
+        (!current_user.nil? or !current_student.nil? or !current_school.nil? or !current_teacher.nil?)
+    end
+
+    def require_login
+        if auth
+            redirect_to user_path
+        end
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,27 +4,35 @@ module ApplicationHelper
             user = current_user
         elsif !current_student.nil?
             user = current_student
-        else
+        elsif !current_teacher.nil?
             user = current_teacher
+        elsif !current_school.nil?
+            user = current_school
         end
+
         user
+    end
+
+    def user_path
+        user = custom_signed_in?
+        path = root_path
+
+        if !user.nil?
+            if user.student?
+                path = home_students_url
+            elsif user.teacher?
+                path = home_teachers_url
+            elsif user.admin?
+                path = home_url
+            elsif user.school?
+                path = home_schools_url
+            end
+        end
+
+        path
     end
 
     def custom_current_user
         custom_signed_in?
-    end
-
-    def custom_logout
-        logout =  nil
-
-        user = custom_signed_in?
-
-        if user.admin?
-            destroy_user_session_path
-        elsif user.student?
-            destroy_student_session_path
-        else
-            destroy_teacher_session_path
-        end
     end
 end
